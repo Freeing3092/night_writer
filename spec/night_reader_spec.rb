3d8552ab6b551e88c6d2bd890c3f39a7a8714d94
Rich Kaht <adamselene1776@protonmail.com>
2022-09-24 00:55:49 -0600

    Test: Add tests for NightReader attribute and behavior

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -11,6 +11,15 @@ RSpec.describe NightReader do
     it "has readable attributes" do
       expect(night_reader.braille_file).to eq('braille.txt')
       expect(night_reader.original_message_file).to eq('original_message.txt')
+      expect(night_reader.contents).to eq(["0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"])
+    end
+  end
+  
+  context 'behavior' do
+    it "#repl_output outputs a terminal message indicating the original
+    message file name and the number of characters" do
+      result = ["0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"]
+      expect(night_reader.repl_output).to eq(result)
     end
   end
 end
