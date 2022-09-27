06f714ed88cb866d6182d1449d2f95594154aa33
Rich Kaht <adamselene1776@protonmail.com>
2022-09-27 10:22:41 -0600

    Feature/Test: Complete functionality for wrapping braille text in the output file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -9,7 +9,6 @@ class NightWriter < BrailleTranslator
     @destination = user_input_array[1]
     @contents = File.open("./data/#{user_input_array[0]}").read
     @unformatted_text = unformatted_text
-    # @text_array = format_text_to_array(@contents.downcase)
     super()
   end
   
@@ -36,15 +35,27 @@ class NightWriter < BrailleTranslator
       braille_chars = file.readlines.map {|line| line.scan(/.{1,2}/)}.transpose
       insert_new_lines_braille(braille_chars)
     end
-    require "pry"; binding.pry
+    @unformatted_text.map! {|row| row.split("\n")}
+    sort_braille_lines
+  end
+  
+  # This method organizes the unformatted_text into the proper row sequence
+  def sort_braille_lines
+    lines_of_braille = @unformatted_text.first.size
+    index = 0
     File.open("./data/#{@destination}", 'w') do |file| 
-      @unformatted_text.each {|line| file.write(line)}
+      lines_of_braille.times do
+        @unformatted_text.each do |row| 
+          file.write(row[index]) 
+          file.write("\n")
+        end
+        index += 1
+      end
     end
   end
   
   # This method inserts new lines at the index position of the first whitespace
   # preceding the 79 index position of each file
-  
   def insert_new_lines_braille(braille_chars)
     index = 0
     new_line_count = @unformatted_text.first.size / 80
@@ -70,19 +81,6 @@ class NightWriter < BrailleTranslator
   # This method returns a boolean value indicating if the argument is uppercase
   def is_uppercase?(character)
     !/[[:upper:]]/.match(character).nil?
-  end
-  
-  # This method takes a string argument and translates to an array with
-  # elements no longer than 40 english characters
-  def format_text_to_array(string)
-    text_array = []
-    until string.empty? do
-      string.size > 40 ? end_line = string.slice(0, 39).rindex(' ') : end_line = (string.size - 1)
-      text_array << string[0..(end_line - 1)] if string.size > 40
-      text_array << string[0..(end_line)] if string.size < 40
-      string.size > 40 ? string = string[(end_line)..-1] : break
-    end
-    text_array.map { |line| line.strip }
   end
   
 end
